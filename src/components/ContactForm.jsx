53c2cc4db58a5f3cf412db473852afa0690f7550
Noxtimo <canbuulo1231@gmail.com>
2024-05-16 12:06:18 +0100

    Added the contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,36 @@
 import React, { useState } from "react";
 
 const ContactForm = () => {
-  return <></>;
+  return (
+    <>
+      <form className="contact-form">
+        {" "}
+        <label>Get In Touch</label>{" "}
+        <input
+          className="label"
+          type="text"
+          name="name"
+          placeholder="Name"
+          required
+        />
+        <input
+          className="label"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <textarea
+          id="message"
+          name="message"
+          rows="4"
+          placeholder="Message"
+          required
+        ></textarea>
+        <input className="submit-button" type="submit" value="Submit" />
+      </form>
+    </>
+  );
 };
 
 export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,15 @@ import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
 import ContactForm from "../components/ContactForm.jsx";
+import { Outlet, Link } from "react-router-dom";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 const Contact = () => {
   return (
     <>
-      <div className="container">
+      <div className="container-contact">
         <div className="content-contact">
           <div className="contact-top">
             <div className="phone">
@@ -21,8 +26,41 @@ const Contact = () => {
             </div>
           </div>
           <div className="contact-bottom">
-            <div className="contact-form"></div>
-            <div className="contact-social"></div>
+            <ContactForm />
+            <div className="contact-social">
+              <div className="message-me">
+                <div>
+                  <h2>Message Me</h2>
+                  <hr></hr>
+                </div>
+                <div>
+                  <h5>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Temporibus consequuntur quod in molestias quia, voluptatibus
+                    voluptatum. Perspiciatis accusantium sapiente magni
+                    recusandae a pariatur aut ea facere saepe reiciendis,
+                    perferendis vitae cum soluta animi optio nihil ex. Beatae,
+                    dignissimos quibusdam est eligendi sit cum? Consequuntur
+                    nesciunt nobis harum alias repellat sequi.
+                  </h5>
+                </div>
+              </div>
+
+              <div className="social-links-contact">
+                <Link to="#">
+                  <FaLinkedin className="social-link-contact" />
+                </Link>
+                <Link to="#">
+                  <FaInstagram className="social-link-contact" />
+                </Link>
+                <Link to="#">
+                  <FaGithub className="social-link-contact" />
+                </Link>
+                <Link to="#">
+                  <FaYoutube className="social-link-contact" />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
